da8d8a4059806cba73f6e34bfe3fa8efc824fc08
vincentmuya <vincentmuya13@gmail.com>
2023-08-22 14:07:12 +0300

    modified main.py MyApp(App) by creating and adding a screen. Created subcategories.kv file App swiching to subcategories screen okay.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
 from landing_page import LandingPage
+from subcategories import SubcategoriesScreen
+
 import requests
 
 
@@ -14,6 +16,12 @@ class MyApp(App):
         landing_page = LandingPage()
         landing_page_screen.add_widget(landing_page)
         self.screen_manager.add_widget(landing_page_screen)
+
+        # Create and add the subcategories screen
+        subcategories_screen_screen = Screen(name='subcategories_screen')
+        subcategories_screen = SubcategoriesScreen('category_id', 'category_name')  # You can pass arguments here if needed
+        subcategories_screen_screen.add_widget(subcategories_screen)
+        self.screen_manager.add_widget(subcategories_screen_screen)
 
         return self.screen_manager
 
