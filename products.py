b8f3c224634d196be029c9695d19846318688860
vincentmuya <vincentmuya13@gmail.com>
2023-09-01 16:21:10 +0300

    Modified products.kv by adding on_touch which calls view_product function which calls the product_details Modified view details function and productCart screen added screen function in view_product

--- products.py
+++ products.py
@@ -1,8 +1,11 @@
-from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
+import requests
+from kivy.app import App
+from kivy.uix.screenmanager import SlideTransition
+
 
 Builder.load_file('products.kv')
 
@@ -11,10 +14,42 @@ class ProductCard(BoxLayout):
     title = StringProperty()
     price = StringProperty()
     image_source = StringProperty()
+    slug = StringProperty()
+    product_id = StringProperty()
 
     def view_product(self):
-        # Define the behavior when the button is pressed
+        # Access the product ID associated with this card
+        product_id = self.product_id
+
+        # Fetch the product details from the API using requests
+        api_url = f"http://localhost:8000/api/product/{product_id}/{self.slug}/"
+        response = requests.get(api_url)
+        print("API Product Details:", response.content)
+        if response.status_code == 200:
+            product_details = response.json()
+            print("Product Details:", product_details)
+            # Now you have the product details, you can pass them to the next screen
+            # Access the ScreenManager and switch to the next screen
+            app = App.get_running_app()
+            product_details_screen = app.root.get_screen('product_details')
+
+            # Load the products into the ProductsScreen instance
+            product_details_screen.load_products(product_details)
+
+            # Transition to the ProductsScreen
+            app.root.transition = SlideTransition(direction='left')
+            app.root.current = 'product_details'
         pass
+        #     # Get the instance of the ProductDetailScreen and update its properties
+        #     product_detail_screen = app.root.ids.product_detail_screen
+        #     product_detail_screen.update_product_details(product_details)
+        # else:
+        #     print("Failed to fetch product details.")
+
+
+class ProductDetailScreen(Screen):
+    title = StringProperty()
+    price = StringProperty()
 
 
 class ProductsScreen(Screen):
@@ -27,5 +62,5 @@ class ProductsScreen(Screen):
 
         for product in products_data:
             product_card = ProductCard(title=product['title'], price=str(product['price']),
-                                       image_source=product['image'])
+                                       image_source=product['image'], product_id=str(product['id']), slug=product['slug'])
             products_layout.add_widget(product_card)
