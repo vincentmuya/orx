f1e4cfadc9f4b199844ccee86764dd3528925400
vincentmuya <vincentmuya13@gmail.com>
2023-09-03 23:11:28 +0300

    added media_url, settings.media_root to save items in media folder in urls.py Added root media to seting and import os

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.urls import re_path as url
 from . import views
 from .views import get_category_id
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^api/product/$', views.ProductList.as_view()),
@@ -11,3 +12,6 @@ urlpatterns = [
     url(r'^api/category-id/(?P<category_name>[\w\-]+)/$', get_category_id, name='get_category_id'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
