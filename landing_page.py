cfc32e161ae447d840b19c7a9850ca905b1d5d5b
vincentmuya <vincentmuya13@gmail.com>
2023-09-15 17:36:19 +0300

    Called Scroll View and added it to the landing page class modified landing page to be scrollable Display screens called verically in boxlayout

--- landing_page.py
+++ landing_page.py
@@ -1,21 +1,42 @@
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.boxlayout import BoxLayout
 from header import Header
 from body_section import BodySection
 from navigation_bar import NavigationBar
-from kivy.uix.label import Label
 from items import ItemsScreen
 
 
-class LandingPage(BoxLayout):
+class LandingPage(ScrollView):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.scroll_type = ['bars']
+        self.bar_width = 10
+        self.do_scroll_x = False  # Disable horizontal scrolling
+        self.do_scroll_y = True   # Enable vertical scrolling
 
-        self.orientation = 'vertical'
+        # Create a vertical BoxLayout to stack the screens
+        self.layout = BoxLayout(orientation='vertical', spacing=10, size_hint_y=None)
+        self.layout.bind(minimum_height=self.layout.setter('height'))
 
-        self.add_widget(Header())
-        self.add_widget(BoxLayout(size_hint_y=None, height=0))
+        # Create and wrap screens with specific heights
+        header = self.wrap_screen(Header(), height=200)
+        body = self.wrap_screen(BodySection(), height=300)
+        items_screen = self.wrap_screen(ItemsScreen(), height=600)
+        nav_bar = self.wrap_screen(NavigationBar(), height=100)
 
-        self.add_widget(BodySection())
-        self.add_widget(ItemsScreen())
-        self.add_widget(NavigationBar())
+        # Add the wrapped screens to the layout
+        self.layout.add_widget(header)
+        self.layout.add_widget(body)
+        self.layout.add_widget(items_screen)
+        self.layout.add_widget(nav_bar)
 
+        # Set the layout as the content of the ScrollView
+        self.add_widget(self.layout)
+
+    def wrap_screen(self, screen, height):
+        """
+        Wrap a screen in a layout with a specific height.
+        """
+        layout = BoxLayout(orientation='vertical', size_hint_y=None, height=height)
+        layout.add_widget(screen)
+        return layout
