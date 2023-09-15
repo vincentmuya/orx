32b92d50458065c088d5e839b9f242013de7d0f8
vincentmuya <vincentmuya13@gmail.com>
2023-09-15 17:57:20 +0300

    created view product method in ItemsCard which displays the product details

--- items.py
+++ items.py
@@ -4,6 +4,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
 from kivy.lang import Builder
+from kivy.uix.screenmanager import SlideTransition
 from kivy.uix.image import AsyncImage
 
 Builder.load_file('items.kv')
@@ -15,6 +16,31 @@ class ItemsCard(BoxLayout):
     image_source = StringProperty()
     slug = StringProperty()
     product_id = StringProperty()
+
+    def view_product(self):
+        # Access the product ID associated with this card
+        product_id = self.product_id
+
+        # Fetch the product details from the API using requests
+        api_url = f"http://localhost:8000/api/product/{product_id}/{self.slug}/"
+        response = requests.get(api_url)
+        print("API Product Details:", response.content)
+        if response.status_code == 200:
+            product_details = response.json()
+            print("Product Details:", product_details)
+            # Now you have the product details, you can pass them to the next screen
+            # Access the ScreenManager and switch to the next screen
+            app = App.get_running_app()
+            product_details_screen = app.root.get_screen('product_details')
+
+            # Load the products into the ProductsScreen instance
+            product_details_screen.load_details(product_details)
+
+            # Transition to the ProductsScreen
+            app.root.transition = SlideTransition(direction='left')
+            app.root.current = 'product_details'
+        else:
+            print("Failed to fetch product details.")
 
 
 class ItemsScreen(Screen):
